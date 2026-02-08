d8e61810cc4b831a00949a8326ee7ade4bdc829b
Jacob <jacoblittler2003@gmail.com>
2026-02-08 10:38:24 -0500

    event_controller can now add events to the db

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -2,4 +2,18 @@ class EventController < ApplicationController
   def index
     @event = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = @user.created_events.build(event_params)
+  end
+
+private
+
+def event_params
+  params.require(:event).permit(:eventname, :location, :date, :time)
+end
 end
